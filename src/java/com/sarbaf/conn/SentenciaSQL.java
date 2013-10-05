1bb89711a4908ceb390876b8382a7be997748c8a
fernando <fernando@fernando-K45VD>
2013-10-05 13:40:52 -0500

    #1 -> Actualización en la clase SentenciaSQL

--- src/java/com/sarbaf/conn/SentenciaSQL.java
+++ src/java/com/sarbaf/conn/SentenciaSQL.java
@@ -7,8 +7,6 @@ package com.sarbaf.conn;
 import com.mysql.jdbc.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
